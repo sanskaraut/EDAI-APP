f375b1d82f16590bde433176c99d37d34e795d47
sanskaraut <sanskaraut12@gmail.com>
2024-10-23 01:42:39 +0530

    page Linking
    SignUp page and expense page linked

--- app/src/main/java/com/cscorner/autohub/MainActivity.java
+++ app/src/main/java/com/cscorner/autohub/MainActivity.java
@@ -2,6 +2,8 @@ package com.cscorner.autohub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page); // Linking main_page.xml
+
+        // Get reference to the ImageButton for expenseManager
+        ImageButton expenseManagerButton = findViewById(R.id.expenseManager);
+
+        // Set an onClickListener to redirect to ExpenseActivity
+        expenseManagerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent to go to ExpenseActivity
+                Intent intent = new Intent(MainActivity.this, ExpenseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
