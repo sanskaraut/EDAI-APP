0f2fe104043fcd415775c184a1f00d121e108d2a
rohitrathod-4004 <rohitrathod4004@gmail.com>
2024-10-23 18:41:40 +0530

    Washing centre connected

--- app/src/main/java/com/cscorner/autohub/MainActivity.java
+++ app/src/main/java/com/cscorner/autohub/MainActivity.java
@@ -2,12 +2,30 @@ package com.cscorner.autohub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton goToWashingPage ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page); // Linking main_page.xml
+
+        goToWashingPage = findViewById(R.id.imageButton5);
+
+        goToWashingPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToWashingPage = new Intent(MainActivity.this , WashingPage.class);
+                startActivity(goToWashingPage);
+
+            }
+        });
     }
 }
